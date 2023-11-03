ea3fb5b53b25bff559fbbae9a5187a860a35f6a0
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-11-03 09:51:04 +0530

    refactor: toggle changes to ml coach

--- src/screens/WorkoutScreen.tsx
+++ src/screens/WorkoutScreen.tsx
@@ -10,9 +10,9 @@ import {
 } from 'react-native';
 import NavBar from '../components/nav/NavBar';
 
-
 const images = {
-  exercise: require('../assets/images/exercise.png'),
+  exercise3D: require('../assets/images/exercise-3d.png'),
+  exerciseML: require('../assets/images/exercise-ml.png'),
   controlBackward: require('../assets/images/control-backward.png'),
   controlPause: require('../assets/images/control-pause.png'),
   controlForward: require('../assets/images/control-forward.png'),
@@ -25,20 +25,32 @@ export default function WorkoutScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ width: '100%' }}></View>
-      <StatusBar style = "light"/>
-      <NavBar/>
+      <StatusBar style="light" />
+      <NavBar />
       <View style={styles.wrapper}>
         <Text style={styles.text}>Exercise 1 of 14</Text>
         <Text style={[styles.text, styles.textHero]}>Stretches</Text>
       </View>
-      <View style={styles.imageWrapper}>
-        <Image source={images.exercise} style={styles.image} />
-      </View>
+      {/* 3D Coach/ML Coach */}
+      {isMLMode ? (
+        <View style={[styles.imageWrapper, { flexGrow: 28 }]}>
+          <Image source={images.exerciseML} style={styles.image} />
+        </View>
+      ) : (
+        <View style={styles.imageWrapper}>
+          <Image
+            source={images.exercise3D}
+            style={[
+              styles.image,
+              { width: '150%', left: '-50%', transform: [{ translateX: 100 }] },
+            ]}
+          />
+        </View>
+      )}
       <View style={styles.controlsWrapper}>
-        <View style={{ flex: 1, justifyContent: 'center' }} />
         {/* ToggleButton */}
         <View style={styles.toggleButtonContainer}>
-          <Text style={styles.text}>3D Couch</Text>
+          <Text style={styles.text}>3D Coach</Text>
           <Switch
             trackColor={{ false: '#767577', true: '#767577' }}
             thumbColor={isMLMode ? '#fff' : '#fff'}
@@ -46,23 +58,25 @@ export default function WorkoutScreen() {
             onValueChange={toggleSwitch}
             value={isMLMode}
           />
-          <Text style={styles.text}>ML Couch</Text>
+          <Text style={styles.text}>ML Coach</Text>
         </View>
         {/* ActionButtons */}
-        <View style={styles.actionButtonContainer}>
-          <Image
-            style={styles.controlActionButton}
-            source={images.controlBackward}
-          />
-          <Image
-            style={styles.controlActionButton}
-            source={images.controlPause}
-          />
-          <Image
-            style={styles.controlActionButton}
-            source={images.controlForward}
-          />
-        </View>
+        {!isMLMode && (
+          <View style={styles.actionButtonContainer}>
+            <Image
+              style={styles.controlActionButton}
+              source={images.controlBackward}
+            />
+            <Image
+              style={styles.controlActionButton}
+              source={images.controlPause}
+            />
+            <Image
+              style={styles.controlActionButton}
+              source={images.controlForward}
+            />
+          </View>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -88,23 +102,29 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   imageWrapper: {
+    flexGrow: 4,
     position: 'relative',
     width: '100%',
-    aspectRatio: 1,
   },
   image: {
-    width: '150%',
-    height: '100%',
-    left: '-50%',
-    transform: [{ translateX: 100 }],
-    resizeMode: 'contain',
     position: 'absolute',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
   },
   controlsWrapper: {
     flex: 1,
     flexDirection: 'column',
     width: '100%',
     padding: 36,
+    // backgroundColor: 'blue',
+  },
+  toggleButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // backgroundColor: 'green',
   },
   actionButtonContainer: {
     flex: 1,
@@ -112,15 +132,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginTop: 48,
+    // backgroundColor: 'yellow',
   },
   controlActionButton: {
     width: 36,
     aspectRatio: 1,
-  },
-  toggleButtonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
   },
 });
